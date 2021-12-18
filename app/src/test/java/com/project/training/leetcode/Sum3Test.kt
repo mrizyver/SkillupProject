dc7304d2604355d99d428704f36fdcaba7952f7a
izyver <mr.izyver@gmail.com>
2021-12-18 18:38:24 +0200

    Created O(n^2) Sum3 algorithm

--- app/src/test/java/com/project/training/leetcode/Sum3Test.kt
+++ app/src/test/java/com/project/training/leetcode/Sum3Test.kt
@@ -10,7 +10,30 @@ class Sum3Test {
             arrayOf(
                 intArrayOf(-1, -1, 2),
                 intArrayOf(-1, 0, 1)
-            ), SquaredSum3.threeSum(intArrayOf(-1, 0, 1, 2, -1, -4))
+            ), InefficientSum3.threeSum(intArrayOf(-1, 0, 1, 2, -1, -4))
+                .map { it.toIntArray() }.toTypedArray()
+        )
+        assertArrayEquals(
+            arrayOf(
+                intArrayOf(-1, -1, 2),
+                intArrayOf(-1, 0, 1)
+            ), EfficientSum3.threeSum(intArrayOf(-1, 0, 1, 2, -1, -4))
+                .map { it.toIntArray() }.toTypedArray()
+        )
+    }
+
+    @Test
+    fun `test 0,0,0`() {
+        assertArrayEquals(
+            arrayOf(
+                intArrayOf(0, 0, 0),
+            ), InefficientSum3.threeSum(intArrayOf(0, 0, 0))
+                .map { it.toIntArray() }.toTypedArray()
+        )
+        assertArrayEquals(
+            arrayOf(
+                intArrayOf(0, 0, 0),
+            ), EfficientSum3.threeSum(intArrayOf(0, 0, 0))
                 .map { it.toIntArray() }.toTypedArray()
         )
     }
